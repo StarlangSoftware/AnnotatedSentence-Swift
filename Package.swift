e8e4e52e0142305dfe8652e97bb93a4cf07d26dc
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-04-09 21:26:15 +0300

    Changed dependency to NamedEntityRecognition

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(name: "DependencyParser", url: "https://github.com/StarlangSoftware/TurkishDependencyParser-Swift.git", .exact("1.0.0")),
         .package(name: "PropBank", url: "https://github.com/StarlangSoftware/TurkishPropBank-Swift.git", .exact("1.0.2")),
         .package(name: "FrameNet", url: "https://github.com/StarlangSoftware/TurkishFrameNet-Swift.git", .exact("1.0.0")),
-        .package(name: "NamedEntityRecognition", url: "https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Swift.git", .exact("1.0.1"))],
+        .package(name: "NamedEntityRecognition", url: "https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Swift.git", .exact("1.0.2"))],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
