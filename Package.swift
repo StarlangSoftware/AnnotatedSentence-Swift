d98109750e16285edef287e377b01503e0e4f0e5
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-26 12:45:28 +0300

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
             targets: ["AnnotatedSentence"]),
     ],
     dependencies: [
-        .package(name: "WordNet", url: "https://github.com/StarlangSoftware/TurkishWordNet-Swift.git", .exact("1.0.2")),
+        .package(name: "WordNet", url: "https://github.com/StarlangSoftware/TurkishWordNet-Swift.git", .exact("1.0.3")),
         .package(name: "SentiNet", url: "https://github.com/StarlangSoftware/TurkishSentiNet-Swift.git", .exact("1.0.4")),
-        .package(name: "DependencyParser", url: "https://github.com/StarlangSoftware/TurkishDependencyParser-Swift.git", .exact("1.0.2")),
+        .package(name: "DependencyParser", url: "https://github.com/StarlangSoftware/TurkishDependencyParser-Swift.git", .exact("1.0.3")),
         .package(name: "PropBank", url: "https://github.com/StarlangSoftware/TurkishPropBank-Swift.git", .exact("1.0.5")),
         .package(name: "FrameNet", url: "https://github.com/StarlangSoftware/TurkishFrameNet-Swift.git", .exact("1.0.1")),
-        .package(name: "NamedEntityRecognition", url: "https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Swift.git", .exact("1.0.5"))],
+        .package(name: "NamedEntityRecognition", url: "https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Swift.git", .exact("1.0.6"))],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
